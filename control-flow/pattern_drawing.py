77259051f71157a06c53cb10c2d2407ff58e9ce5
Tsh3py <1tshepi.mohitshane@gmail.com>
2025-09-21 19:34:45 +0200

    feat:Drawing Patterns with Nested Loops

---
+++ control-flow/pattern_drawing.py
@@ -0,0 +1,23 @@
+try:
+    size = int(input("Enter the size of the pattern: "))
+    
+    if size <= 0:
+        print("Please enter a positive integer.")
+        exit()
+except ValueError:
+    print("Invalid input. Please enter a positive integer.")
+    exit()
+
+
+row = 0
+
+
+while row < size:
+    
+    for _ in range(size):
+        
+        print("*", end="")
+    
+    print()
+    
+    row += 1
